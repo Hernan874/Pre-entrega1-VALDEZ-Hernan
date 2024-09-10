b506829e94069527c5b4a77977a901365d355057
hernan874 <hernanvaldez874@gmail.com>
2024-09-10 20:27:11 -0300

    actualizado-proyecto

--- js/app.js
+++ js/app.js
@@ -1,4 +1,5 @@
 
+/*
 const nombre = prompt("Ingrese su nombre:");
 
 
@@ -40,3 +41,44 @@ if (op===3){
 while (op > 3){
     alert("no ingreso ninguna operacion a realizar")
 }
+
+let total = 0;
+for (let i =1;i<=10;i++){
+    total+= i;
+};
+console.log(total);
+*/
+
+const producto = {
+    nombre:"cactus",
+    categoria:"plantas",
+    precio:10000
+}
+class Producto2{
+    constructor(nombre, categoria, precio) {
+        this.nombre = nombre;
+        this.categoria   = categoria;
+        this.precio  = precio;
+    }
+        mostrarprecio(){ console.log("el precio del producto "+ this.nombre+ "es "+ this.precio);}
+}
+const producto1 = new Producto2("Homero", "Av. Siempreviva 742", 200);
+const producto2 = new Producto2("Marge", "Av. Siempreviva 742", 200);
+producto1.mostrarprecio();
+producto2.mostrarprecio();
+
+const arrayproductos=[]
+let   cantidad     = 5;
+do{
+   let entrada = prompt("Ingresar nombre");
+   arrayproductos.push(entrada.toUpperCase());
+   console.log(arrayproductos.length);
+}while(arrayproductos.length != cantidad);
+alert("Lista de productos: \n" + arrayproductos.join("\n"));
+arrayproductos.push(producto);
+console.log(arrayproductos);
+
+let busnom = prompt("ingrese nombre del producto a buscar");
+const buscado = arrayproductos.find(producto => producto.nombre === busnom);
+console.log(buscado);
+console.log(Math.min(arrayproductos));
