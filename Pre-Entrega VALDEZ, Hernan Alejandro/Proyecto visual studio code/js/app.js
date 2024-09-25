a985727ab11045dee7387ab122500a87daa9c702
Hernan874 <hernanvaldez874@gmail.com>
2024-09-25 18:02:50 -0300

    Add files via upload

---
+++ Pre-Entrega VALDEZ, Hernan Alejandro/Proyecto visual studio code/js/app.js
@@ -0,0 +1,132 @@
+/*
+const nombre = prompt("Ingrese su nombre:");
+
+
+function saludar(x){
+    alert("Bienvenido "+ x);
+}
+
+saludar(nombre);
+
+alert("A continuacion ingrese dos numeros para realizar distintas operaciones");
+let num1 = parseInt(prompt("Ingrese el primer numero:"));
+let num2 = parseInt(prompt("Ingrese el segundo numero:"));
+
+function suma(x,y){
+    let resultado = x + y;
+    alert("el resultado de la operacion es: "+ resultado);
+}
+function resta(x,y){
+    let resultado = x - y;
+    alert("el resultado de la operacion es: "+ resultado);
+}
+function multiplicacion(x,y){
+    let resultado = x * y;
+    alert("el resultado de la operacion es: "+ resultado);
+}
+
+let op = parseInt(prompt("Ingrese operacion a realizar (1.suma 2.resta 3.multiplicacion):" ));
+
+if (op===1){
+    suma(num1,num2);
+}
+if (op===2){
+    resta(num1,num2);
+}
+if (op===3){
+    multiplicacion(num1,num2);
+}
+
+while (op > 3){
+    alert("no ingreso ninguna operacion a realizar")
+}
+
+let total = 0;
+for (let i =1;i<=10;i++){
+    total+= i;
+};
+console.log(total);
+
+
+const producto = {
+    nombre:"cactus",
+    categoria:"plantas",
+    precio:10000
+}
+class Producto2{
+    constructor(nombre, categoria, precio) {
+        this.nombre = nombre;
+        this.categoria   = categoria;
+        this.precio  = precio;
+    }
+        mostrarprecio(){ console.log("el precio del producto "+ this.nombre+ "es "+ this.precio);}
+}
+const producto1 = new Producto2("lapacho", "planta", 5000);
+const producto2 = new Producto2("pala", "herramienta", 8000);
+producto1.mostrarprecio();
+producto2.mostrarprecio();
+
+const arrayproductos=[]
+let   cantidad     = 5;
+do{
+   let entrada = prompt("Ingresar nombre");
+   arrayproductos.push(entrada.toUpperCase());
+   console.log(arrayproductos.length);
+}while(arrayproductos.length != cantidad);
+alert("Lista de productos: \n" + arrayproductos.join("\n"));
+arrayproductos.push(producto);
+console.log(arrayproductos);
+
+let busnom = prompt("ingrese nombre del producto a buscar");
+const buscado = arrayproductos.find(producto => producto.nombre === busnom);
+console.log(buscado);
+console.log(Math.min(arrayproductos));*/
+
+
+
+
+class Producto2{
+    constructor(nombre, descripcion, precio,) {
+        this.nombre = nombre;
+        this.categoria   = descripcion;
+        this.precio  = precio;
+        
+    }
+}
+
+const botones = document.querySelectorAll('.btn-com');
+
+let carrito = [];
+let carritoEnLS = JSON.parse(localStorage.getItem('carrito-com'))
+
+if (carritoEnLS) {
+    carrito=carritoEnLS
+    alert('tenes articulos en el carrito para comprar')
+}
+
+function manejarClick(event) {
+
+  const boton = event.target;
+
+  const card = boton.closest('.card-body');
+
+  const nombre = card.querySelector('.card-title').textContent;
+  const descripcion = card.querySelector('.card-text').textContent;
+  const precio = parseFloat(card.querySelector('.precio-text').textContent);
+
+  const producto = new Producto2(nombre, descripcion, precio);
+
+  carrito.push(producto);
+
+  
+  console.log('Producto añadido:', producto);
+  console.log('Carrito actual:', carrito);
+  localStorage.setItem('carrito-com', JSON.stringify(carrito));
+}
+
+botones.forEach(boton => {
+  boton.addEventListener('click', manejarClick);
+});
+
+
+
